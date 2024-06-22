d7b4314e0cad1668c99ce0e7e83c0a1965fb6b06
Antônio Coelho <132955062+AntonioSCoelho97@users.noreply.github.com>
2024-06-22 16:08:12 -0300

    Update app_pycaret.py

--- Modulo_38/Projeto_Final/app_pycaret.py
+++ Modulo_38/Projeto_Final/app_pycaret.py
@@ -225,7 +225,8 @@ def main():
         st.dataframe(info_df)
 
         st.header('Carregando o modelo')
-        model = load_model('lightgbm_model_final')
+        url = 'https://raw.githubusercontent.com/AntonioSCoelho97/EBAC-Curso/main/Modulo_38/Projeto_Final/lightgbm_model_final.pkl'
+        model = load_model(url)
         st.write(model)
 
         st.write('Identificando o modelo carregado')
