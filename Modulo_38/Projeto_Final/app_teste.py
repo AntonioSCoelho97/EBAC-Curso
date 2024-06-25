12b725d238a93632c9c4d0ef2bb53498ac933c96
Antônio Coelho <132955062+AntonioSCoelho97@users.noreply.github.com>
2024-06-24 21:42:40 -0300

    Add files via upload

---
+++ Modulo_38/Projeto_Final/app_teste.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+from pycaret.classification import *
+import os
+
+if os.path.exists('dataset.csv'):
+    df = pd.read_csv('dataset.csv', index_col=None)
+
+with st.sidebar:
+    st.title('Teste Pycaret')
+    choice = st.radio('Selecione uma das opções:', ['Arquivo', 'Previsão'])
+
+if choice == 'Arquivo':
+    st.title('Carregue seu dataset')
+    file = st.file_uploader('Carregue seu dataset')
+    if file:
+        df = pd.read_csv(file, index_col=None)
+        df.to_csv('dataset.csv', index=None)
+        st.dataframe(df)
+
+if choice == 'Previsão':
+    with open('./lightgbm_model_final.pkl', 'rb') as f:
+        st.download_button('Download do modelo treinado', f, file_name='lightgbm_model_final.pkl')
