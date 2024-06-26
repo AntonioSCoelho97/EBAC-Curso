90473336191ace425c45a87945c27689b5f61b84
Antônio Coelho <132955062+AntonioSCoelho97@users.noreply.github.com>
2024-06-25 21:01:09 -0300

    Update app_pycaret.py

--- Modulo_38/Projeto_Final/app_pycaret.py
+++ Modulo_38/Projeto_Final/app_pycaret.py
@@ -227,7 +227,7 @@ def main():
         st.dataframe(info_df)
 
         st.header('Carregando o modelo')
-        url = 'https://github.com/AntonioSCoelho97/EBAC-Curso/raw/main/Modulo_38/Projeto_Final/lightgbm_model_final.pkl'
+        url = 'https://raw.githubusercontent.com/AntonioSCoelho97/EBAC-Curso/main/Modulo_38/Projeto_Final/lightgbm_model_final.pkl'
         token = 'Streamlit-Pycaret'
         headers = {'Authorization': f'token {token}'}
         response = requests.get(url, headers=headers)
