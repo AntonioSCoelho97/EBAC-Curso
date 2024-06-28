575213a341c377e180346a9003388af19161a29d
Antônio Coelho <132955062+AntonioSCoelho97@users.noreply.github.com>
2024-06-28 20:10:36 -0300

    Update app_teste.py

--- Modulo_38/Projeto_Final/app_teste.py
+++ Modulo_38/Projeto_Final/app_teste.py
@@ -40,7 +40,7 @@ if choice == 'Arquivo':
 
 if choice == 'Previsão':
     file_pkl = st.file_uploader('Carregue seu arquivo pkl')
-    st.write(file_pkl.name)
+    st.write(os.path.abspath(file_pkl.name))
     if file_pkl:
         pkl_path = Path(__file__).parents[1] / 'Modulo_38/Projeto_Final/lightgbm_model_final'
         model = load_model(pkl_path)
