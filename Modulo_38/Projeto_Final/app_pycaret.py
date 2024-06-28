207f481ec74d11e88c8812dee7bcab309ffba7a8
Antônio Coelho <132955062+AntonioSCoelho97@users.noreply.github.com>
2024-06-28 19:49:51 -0300

    Update app_pycaret.py

--- Modulo_38/Projeto_Final/app_pycaret.py
+++ Modulo_38/Projeto_Final/app_pycaret.py
@@ -226,10 +226,10 @@ def main():
         st.dataframe(info_df)
 
         st.header('Carregando o modelo')
-        link_pkl = 'https://github.com/AntonioSCoelho97/EBAC-Curso/blob/54ccd349025e310a5cf00ca390af7c4d80d4f748/Modulo_38/Projeto_Final/lightgbm_model_final.pkl'
-        data = pd.read_pickle(link_pkl)
-        st.write(data)
-        # model = load_model(link_pkl, 'rb')
+        link_pkl = 'https://api.github.com/users/AntonioSCoelho97/EBAC-Curso/Modulo_38/Projeto_Final/lightgbm_model_final.pkl'
+        # data = pd.read_pickle(link_pkl)
+        # st.write(data)
+        model = load_model(link_pkl, 'rb')
         # model = load_model('./lightgbm_model_final.pkl', 'rb', platform = 'aws', authentication = {'bucket' : 'XXX'})
         st.write(model)
 
